287498b99c56a4b0112f0e77010ff971e191e825
23408639379 <ext_mheavey@buenosaires.com>
2023-02-27 16:13:04 -0300

    flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ShapeFlow from './components/Shapes';
+import AddButton from './components/AddButton';
+import ReactDragNDrop from './components/ReactDragNdrop';
+import ExpandAndCollapse from './components/ExpandAndCollapse';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error,
+      errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return <>Some thing wrong</>;
+    }
+    return this.props.children;
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <AddButton/>
+      {/* <ShapeFlow/> */}
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/utils/ReactFlowUtils.js
@@ -0,0 +1,11 @@
+
+
+const findFirstNode (nodes) => {
+    const firstNode = nodes.find(item=>item.id===initialEdge.target)
+      setEdges((eds) => eds.concat({
+        ...initialEdge,
+        // source: parentNode.id,
+      }));
+      setAddnode(false);
+      setParentNode(null);
+}
